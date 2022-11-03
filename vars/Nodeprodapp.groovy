27058526db068ad9111753f20e11fcf8bc783ab3
Akash221997 <114996063+Akash221997@users.noreply.github.com>
2022-11-03 11:27:28 +0530

    Update Nodeprodapp.groovy

--- vars/Nodeprodapp.groovy
+++ vars/Nodeprodapp.groovy
@@ -7,7 +7,7 @@ withEnv(["AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}", "AWS_SECRET_ACCESS_KEY=${
           sh 'docker tag jenkins-pipeline-build:latest 927491280662.dkr.ecr.eu-west-2.amazonaws.com/jenkins-pipeline-build:latest'
           sh 'docker push 927491280662.dkr.ecr.eu-west-2.amazonaws.com/jenkins-pipeline-build:latest'
           sh 'ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/dev-server.pem ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 927491280662.dkr.ecr.eu-west-2.amazonaws.com'
-          sh 'ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/dev-server.pem ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com sudo docker pull 927491280662.dkr.ecr.eu-west-2.amazonaws.com/jenkins-pipeline-build:latest'
+          sh 'ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/dev-server.pem ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com docker pull 927491280662.dkr.ecr.eu-west-2.amazonaws.com/jenkins-pipeline-build'
           sh 'scp -i "/var/lib/jenkins/dev-server.pem" -r /var/lib/jenkins/workspace/nodejs-docker-compose-test/docker-compose.yml ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com:/home/ec2-user'
           sh 'ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/dev-server.pem ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com sudo docker-compose up'
 }

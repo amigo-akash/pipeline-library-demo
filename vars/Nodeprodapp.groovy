478f2b82f52d2a4c0cde7fcd0e1b2e7ab0f6f0b8
Akash221997 <114996063+Akash221997@users.noreply.github.com>
2022-11-04 12:24:39 +0530

    Update Nodeprodapp.groovy

--- vars/Nodeprodapp.groovy
+++ vars/Nodeprodapp.groovy
@@ -9,8 +9,12 @@ withEnv(["AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}", "AWS_SECRET_ACCESS_KEY=${
           sh 'ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/dev-server.pem ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 927491280662.dkr.ecr.eu-west-2.amazonaws.com'     
           sh 'scp -i "/var/lib/jenkins/dev-server.pem" -r /var/lib/jenkins/workspace/nodejs-docker-compose-test/docker-compose.yml ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com:/home/ec2-user'
           sh 'ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/dev-server.pem ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com sudo docker-compose up'
-          sh 'ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/dev-server.pem ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com sudo docker-compose down'
-}
+    post {
+        Always {
+          sh 'ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/dev-server.pem ec2-user@ec2-35-176-254-143.eu-west-2.compute.amazonaws.com sudo docker-compose down --remove-orphans'
+        }
+    }
+  }
 }
 catch(e) {}  
 }

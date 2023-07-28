2dd18af20779d35d5c0fd7569d44fe967dce146a
Akash221997 <114996063+Akash221997@users.noreply.github.com>
2023-07-28 12:05:23 +0530

    Create deployfrontend.groovy

---
+++ vars/deployfrontend.groovy
@@ -0,0 +1,10 @@
+def call ()
+{
+   try {
+withAWS(credentials: 'Tweeny_IAM', region: 'ap-south-1') {
+        sh ('aws eks update-kubeconfig --name t-cluster-1 --region ap-south-1')
+        sh 'helm upgrade --install pluto-frontend ./pluto_app_config/pluto_helm_chart/ --set frontend.enabled=true --set frontend.image.repository=927491280662.dkr.ecr.ap-south-1.amazonaws.com/pluto-frontend'
+}
+}
+catch(e) {}
+}

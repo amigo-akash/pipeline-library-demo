6b15d3debe49371d2fafaf1362fdeec6feaf70f2
Akash221997 <114996063+Akash221997@users.noreply.github.com>
2023-07-28 12:55:08 +0530

    Update deployfrontend.groovy

--- vars/deployfrontend.groovy
+++ vars/deployfrontend.groovy
@@ -1,7 +1,6 @@
 def call ()
 {
-   git url: 'https://gitlab.com/dev-products1/pluto-poc-frontend.git', branch: 'docker', credentialsId: 'my-gitlab-repo-creds'
-   try {
+     try {
 withAWS(credentials: 'Tweeny_IAM', region: 'ap-south-1') {
         sh ('aws eks update-kubeconfig --name t-cluster-1 --region ap-south-1')
         sh 'helm upgrade --install pluto-frontend ./pluto_app_config/pluto_helm_chart/ --set frontend.enabled=true --set frontend.image.repository=927491280662.dkr.ecr.ap-south-1.amazonaws.com/pluto-frontend'

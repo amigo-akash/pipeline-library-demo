71668950911baf303bbd5fb818e28490f262864d
Akash221997 <114996063+Akash221997@users.noreply.github.com>
2023-07-28 12:04:02 +0530

    Create deploybackend.groovy

---
+++ vars/deploybackend.groovy
@@ -0,0 +1,10 @@
+def call ()
+{
+   try {
+withAWS(credentials: 'Tweeny_IAM', region: 'ap-south-1') {
+        sh ('aws eks update-kubeconfig --name t-cluster-1 --region ap-south-1')
+        sh 'helm upgrade --install pluto-backend ./pluto_app_config/pluto_helm_chart/ --set backend.enabled=true --set backend.image.repository=927491280662.dkr.ecr.ap-south-1.amazonaws.com/pluto-app'
+}
+}
+catch(e) {}
+}

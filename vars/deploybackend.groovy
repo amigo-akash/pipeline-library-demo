8484481b6620d1891ca63fdf14a7ae84fa69dcba
Akash221997 <114996063+Akash221997@users.noreply.github.com>
2023-07-28 12:35:49 +0530

    Update deploybackend.groovy

--- vars/deploybackend.groovy
+++ vars/deploybackend.groovy
@@ -1,5 +1,6 @@
 def call ()
 {
+   git url: 'https://gitlab.com/dev-products1/pluto-poc-backend.git', branch: 'docker', credentialsId: 'my-gitlab-repo-creds'
    try {
 withAWS(credentials: 'Tweeny_IAM', region: 'ap-south-1') {
         sh ('aws eks update-kubeconfig --name t-cluster-1 --region ap-south-1')
